b5a70f60dd2d6d0efb6cd8694c90ce7d7a14da1b
Jacek <jacek.kata@gmail.com>
2020-06-13 12:57:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import setup
 
 setup(name='gym_fx',
       version='0.0.1',
-      install_requires=['gym']#And any other dependencies required
-      install_requires=['ta']
+      install_requires=['gym']['ta']#And any other dependencies required
 )
